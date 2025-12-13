86bd5f84f816cc29cf4a337d3a3810b6a5c3e174
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2025-12-12 23:05:09 -0500

    add-changes

--- src/components/global/Componentes/index.ts
+++ src/components/global/Componentes/index.ts
@@ -1,5 +1,6 @@
 export { default as MenuView } from "./MenuView"
 export { default as DescargarApp } from "./DescargarApp"
+export { default as HeroSesion } from "./HeroSesion.astro"
 
 
 
--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,4 +1,4 @@
 export { Header } from "./Header"
 export { Layout } from "./Layout"
 export { LayoutGeneral } from "./LayoutGeneral"
-export {  MenuView, DescargarApp} from "./Componentes"
+export {  MenuView, DescargarApp, HeroSesion} from "./Componentes"
