d9a68569bc1cb1f60bbe0c546425872012d20e88
miguelyanpo-dev <miguel.yanpo@gmail.com>
2026-01-20 08:37:10 -0500

    add-changes

--- src/components/global/Componentes/index.ts
+++ src/components/global/Componentes/index.ts
@@ -3,6 +3,7 @@ export { default as DescargarApp } from "./DescargarApp"
 export { default as HeroSesion } from "./HeroSesion.astro"
 export { default as CategoriasDestacados } from "./CategoriasDestacados.astro"
 export { default as PorqueElegirnos } from "./PorqueElegirnos.astro"
+export { default as TodosLosProductos } from "./TodosLosProductos.astro"
 
 
 
